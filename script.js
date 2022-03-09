23a440ad60634bc263fc58b458dd1c404513fbc1
Ankit luitel <ankitluitel@Ankit.local>
2022-03-09 18:28:37 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+// ..news Section...
+// $(document).ready(function(){
+//     $("#news-slider").owlCarousel({
+//         items:3,
+//         navigation:true,
+//         navigationText:["",""],
+//         autoPlay:true
+//     });
+// });
+// var list=document.getElementById("list");
+// list.style.maxHeight = "0px";
+// function menutap(){
+
+// if (list. style.maxHeight == "0px")
+// {
+// list.style.maxHeight="500px";
+// }
+// else{
+//     list.style.maxHeight="0px";
+// }
+
+// }
+
+// function menutap() {
+//   var x = document.getElementById("nav");
+//   if (x.className === "nav-item") {
+//     x.className += " responsive";
+//   } else {
+//     x.className = "nav-item"
+//   }
+// }
+
+
+
